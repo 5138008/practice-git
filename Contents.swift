496fc619e858c4364c192875189a368626e4ffc6
Shirui Zhong <shirui.us@gmail.com>
2019-07-08 11:53:26 -0700

    First commit

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//define interger and double variables, working with intergers and doubles
+//data types: ints and doubles
+var int = 4
+var double = 5.0
+
+print (Double(int) * double);
+
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98
+
+print(((a + b + c) +Double(d)) / 4)
